6b4e7a5d08db9780555887629accd2b622ee7bcd
Thunderheads <65041451+Thunderheads@users.noreply.github.com>
2022-03-28 20:47:26 +0200

    modification indentation, ajout de la page login, modification des routes

---
+++ src/app/sortie/afficher-user/afficher-user.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AfficherUserComponent } from './afficher-user.component';
+
+describe('AfficherUserComponent', () => {
+  let component: AfficherUserComponent;
+  let fixture: ComponentFixture<AfficherUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AfficherUserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AfficherUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
